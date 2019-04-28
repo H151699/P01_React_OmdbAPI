57b3f6a959c5c49da3413e57e533a67a74e3d55a
h151699 <h151699@stud.hvl.no>
2019-04-28 21:52:40 +0200

    created dummy movie data

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,20 @@ class App extends React.Component<any, any> {
 
     
     const movies = [
-      {id: "0", title: "batman", Year_of_release: 1888, type: "movie"},
-      {id: "1", title: "wnderwoman", Year_of_release: 2018, type: "movie"}
+      {id: "0", title: "batman", year: 1888, released: "movie", runtime:"136 min", Genre:"Action, Adventure, Comedy, Sci-Fi", 
+        director:"Chris Pratt, Zoe Saldana, Dave Bautista, Vin Diesel",Language:"English", poster_src:"https://m.media-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg" },
+      
+      {id: "1", title: "wonderwoman", year: 1888, released: "movie", runtime:"136 min", Genre:"Action, Adventure, Comedy, Sci-Fi", 
+        director:"Chris Pratt, Zoe Saldana, Dave Bautista, Vin Diesel",Language:"English", poster_src:"https://m.media-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg" },
+      
 
     ]
+    this.state={
+      headRows:[
+        'title',
+        
+      ]
+    }
 
     this.state ={rows:[
       <p key="0"> this is my row0</p>,
@@ -32,10 +42,42 @@ class App extends React.Component<any, any> {
 
     var movieRows:any = []
     movies.forEach((movie)=> {
-      console.log(movie.title);
+      //console.log(movie.title);
+    
 
-      movieRows.push(<p key = {movie.id}
-      > movie title: {movie.title}</p>)
+      const movieRow = <table key={movie.id}>
+        <thead>
+          <tr>
+            <td> ID </td>
+            <td> Title </td>
+            <td> Year </td>
+            <td> Runtime </td>
+            <td> Genre </td>
+            <td> Director </td>
+            <td> Language </td>
+            <td> Released </td>
+            <td> Poster </td>
+
+          </tr>
+
+        </thead>
+        <tbody>
+          <tr>
+            <td>{movie.id}</td>
+            <td>{movie.title}</td>
+            <td>{movie.year} </td>
+            <td>{movie.runtime}</td>
+            <td>{movie.Genre}</td>
+            <td>{movie.director}</td>
+            <td>{movie.Language}</td>
+            <td>{movie.released}</td>
+            <td><img alt="poster" width="50" src={movie.poster_src}/></td>
+          </tr>
+        </tbody>
+
+        </table>
+      // movieRows.push(<p key = {movie.id}> movie title: {movie.title}</p>)
+      movieRows.push(movieRow)
     })
 
     this.state = {rows: movieRows}
@@ -57,7 +99,7 @@ class App extends React.Component<any, any> {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            OMDB MOVIE API
           </p>
           <a
             className="App-link"
@@ -71,7 +113,7 @@ class App extends React.Component<any, any> {
             <tbody>
               <tr>
                 <td>
-
+                  
                 </td>
               </tr>
             </tbody>
@@ -82,6 +124,7 @@ class App extends React.Component<any, any> {
         </header>
 
         {this.state.rows}
+       
         
       </div>
     );
